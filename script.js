10bd84cebe2c83d28b87e0180728d86171852636
Julian Vela <vela0052@algonquinlive.com>
2022-02-06 17:01:22 -0600

    Javascript finished

---
+++ script.js
@@ -0,0 +1,21 @@
+const $emojis = document.getElementById('emojis')
+const emojis = []
+for(let i=13; i<=91; i++){
+    emojis.push(`<div class="emojibox"><p style="text-align: center;">
+    <span style="font-size: 2rem;">&#1278${i};</span><br>
+    <br>
+    <code>1278${i}</code>
+  </p></div>`)
+    emojis.push(`<div class="emojibox"><p style="text-align: center;">
+    <span style="font-size: 2rem;">&#1285${i};</span><br>
+    <br>
+    <code>1285${i}</code>
+  </p></div>`)
+  
+}
+
+
+console.log(emojis)
+
+$emojis.innerHTML += emojis.join('')
+console.log($emojis.innerHTML)
